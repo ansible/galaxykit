e5144c31224a041b163c85b04229ac2fb9dcce47
Henderson Hummel <hhummel@redhat.com>
2021-05-11 13:34:49 -0700

    Release 0.0.1 - update setup.py for first release (#5)
    * Update setup.py with final bits of info
    
    * Typo in setup.py
    
    * Update setup.py
    
    * bump version to 0.0.1

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
+#!/usr/bin/env python3
+"""
+setuptools script for installing galaxykit
+"""
+
+import pathlib
 from setuptools import setup
 
-setup(
-        name='galaxy-kit',
-        version='0.0.1',
-        description='todo',
-        author='Henderson Hummel',
-        author_email='hhummel@redhat.com',
-        license='MIT',
-        packages={'galaxy-kit'}
-)
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
 
+setup(
+    author="Red Hat PEAQE Team",
+    description="A small client library for testing galaxy_ng.",
+    license="GPLv2",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    name="galaxykit",
+    packages={"galaxykit"},
+    url="https://github.com/hendersonreed/galaxykit/",
+    version="0.0.1",
+    install_requires=["requests", "simplejson"],
+    extra_requires={"dev": ["black", "pylint", "flake8"]},
+)
