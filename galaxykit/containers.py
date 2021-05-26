aaedd1bacc73834a40ca87f48d318124a236308a
Calvin Spealman <cspealma@redhat.com>
2021-05-26 10:18:34 -0400

    Add delete_permissions helper, add containers module with set_readme/get_readme, add support for group perms and container readme in command line

---
+++ galaxykit/containers.py
@@ -0,0 +1,9 @@
+def get_readme(client, container):
+    url = f"_ui/v1/execution-environments/repositories/{container}/_content/readme/"
+    return client.get(url)
+
+def set_readme(client, container, readme):
+    url = f"_ui/v1/execution-environments/repositories/{container}/_content/readme/"
+    resp = get_readme(client, container)
+    resp["text"] = readme
+    return client.put(url, resp)
--- galaxykit/groups.py
+++ galaxykit/groups.py
@@ -35,6 +35,10 @@ def delete_group(client, group_name):
     delete_url = f"_ui/v1/groups/{group_id}"
     return client.delete(delete_url, parse_json=False)
 
+def get_permissions(client, group_name):
+    group_id = get_group(client, group_name)["id"]
+    permissions_url = f"_ui/v1/groups/{group_id}/model-permissions/"
+    return client.get(permissions_url)
 
 def set_permissions(client, group_name, permissions):
     """
@@ -55,6 +59,28 @@ def set_permissions(client, group_name, permissions):
         payload = {"permission": perm}
         client.post(permissions_url, payload)
         # TODO: Check the results of each and aggregate for a return value
+
+def delete_permissions(client, group_name, permission):
+    """
+    Assigns the given permissions to the group.
+    `permissions` must be a list of strings, each one recognized as a permission by the backend. See
+    them listed at the link below:
+    https://github.com/ansible/galaxy_ng/blob/ca503375077a225a5fb215e6fb2c6ae47e09cfd7/galaxy_ng/app/api/ui/serializers/user.py#L122
+
+    Container permissions are in another file:
+    https://github.com/ansible/galaxy_ng/blob/009385fb3a1a34d1df9ff369e2e15c3fa27869b3/galaxy_ng/app/access_control/statements/pulp_container.py#L139
+
+    The permissions are the ones that match the "namespace.permission-name" format.
+
+    """
+    group_id = get_group(client, group_name)["id"]
+    permissions_url = f"_ui/v1/groups/{group_id}/model-permissions/"
+    resp = client.get(permissions_url)
+    for perm in resp["data"]:
+        if perm["permission"] == permission:
+            perm_id = perm["id"]
+            perm_url = f"_ui/v1/groups/{group_id}/model-permissions/{perm_id}/"
+            client.delete(perm_url, parse_json=False)
 
 def get_group_list(client):
     """
