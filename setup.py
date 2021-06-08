64bc98464a6e1a54c3e1b1bd75ad2f4f5fab2692
Henderson Hummel <hhummel@redhat.com>
2021-06-08 13:23:03 -0700

    Bump version (breaking changes in next release) (#12)

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     name="galaxykit",
     packages={"galaxykit"},
     url="https://github.com/hendersonreed/galaxykit/",
-    version="0.2.1",
+    version="0.3.0",
     install_requires=["requests", "simplejson"],
     extra_requires={"dev": ["pre-commit"]},
     entry_points={
