bd2dfe387dcb4cee71ccbb4c8d0426fda9b15e3d
Henderson Hummel <hhummel@redhat.com>
2021-08-25 11:14:29 -0700

    Minor reformatting, add comments to artifact_upload

--- galaxykit/collections.py
+++ galaxykit/collections.py
@@ -43,20 +43,29 @@ def upload_artifact(
     hash=True,
     no_filename=False,
     no_file=False,
-    use_distribution=True,
 ):
     """
     Publishes a collection to a Galaxy server and returns the import task URI.
 
-    :param collection_path: The path to the collection tarball to publish.
+    :param config: a configuration object to describe the artifact to be uploaded.
+        Example:
+            {"namespace": "foo", "name": "bar"}
+    :param client: a GalaxyClient object. Must be authenticated.
+    :param artifact: the collection artifact to be uploaded. Expects structure to be that
+        of collections produced using the orionutils build_collection function.
+    :param hash: If false, no hash is produced. If true, hash is generated on the fly.
+        Alternatively, could pass a hash in directly as this argument.
+    :param no_filename: If True, no filename is attached to the request.
+    :param no_file: If True, no file is attached to the request.
     :return: The import task URI that contains the import results.
     """
-    collection_path = artifact.filename
-    with open(collection_path, "rb") as collection_tar:
-        data = collection_tar.read()
 
     def to_bytes(s, errors=None):
         return s.encode("utf8")
+
+    collection_path = artifact.filename
+    with open(collection_path, "rb") as collection_tar:
+        data = collection_tar.read()
 
     boundary = "--------------------------%s" % uuid.uuid4().hex
     file_name = os.path.basename(collection_path)
@@ -66,17 +75,20 @@ def upload_artifact(
 
     if hash:
         if isinstance(hash, bytes):
+            # if a pre-computed hash is passed in, just use that.
             b_hash = hash
         else:
+            # otherwise hash the collection contents.
             from hashlib import sha256
 
             b_hash = to_bytes(sha256(data).hexdigest(), errors="surrogate_or_strict")
 
-        form.extend(
-            [part_boundary, b'Content-Disposition: form-data; name="sha256"', b_hash]
-        )
+        # add the hash to the request.
+        form.extend([part_boundary, b'Content-Disposition: form-data; name="sha256"', b_hash])
 
+    # only add the file to the request if no_file == False
     if not no_file:
+        # skip the filename if no_filename
         if no_filename:
             form.extend(
                 [
@@ -89,8 +101,7 @@ def upload_artifact(
             form.extend(
                 [
                     part_boundary,
-                    b'Content-Disposition: file; name="file"; filename="%s"'
-                    % to_bytes(file_name),
+                    b'Content-Disposition: file; name="file"; filename="%s"' % to_bytes(file_name),
                     b"Content-Type: application/octet-stream",
                 ]
             )
