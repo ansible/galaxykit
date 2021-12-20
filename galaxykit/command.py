75cccba3066683b329a66fe0dd645efb49566de5
Henderson Hummel <hhummel@redhat.com>
2021-12-20 09:48:33 -0800

    `user` command now parses as it should.

--- galaxykit/command.py
+++ galaxykit/command.py
@@ -2,118 +2,92 @@ import argparse
 import sys
 import json
 
-from .client import GalaxyClient, GalaxyClientError
-from . import containers
-from . import collections
-from . import groups
-from . import namespaces
-from . import users
 
-EXIT_OK = 0
-EXIT_UNKNOWN_ERROR = 1
-EXIT_NOT_FOUND = 2
-EXIT_DUPLICATE = 4
-
-
-def print_unknown_error(args):
-    print(f"Unknown {args.object} operation '{args.operation}'")
-    sys.exit(EXIT_UNKNOWN_ERROR)
-
-
-def format_list(data, identifier):
-    buffer = []
-    for datum in data:
-        line = [datum[identifier]]
-        for key, value in datum.items():
-            if key != identifier and value:
-                s = f"{key}={value}"
-                line.append(s)
-        buffer.append(" ".join(line))
-    return "\n".join(buffer)
-
-
-def report_error(resp):
-    if "errors" in resp:
-        for error in resp["errors"]:
-            print(
-                f"API Failure: HTTP {error['status']} {error['code']}; {error['title']} ({error['detail']})"
-            )
-
-
-def user(parent_parser, client):
-    parser = argparse.ArgumentParser(parent=parent_parser)
-    args = parser.parse_args(parent_parser.rest)
+def parse_collections(subparsers):
     return None
 
 
-def namespace(parent_parser, client):
+def parse_containers(subparsers):
     return None
 
 
-def collection(parent_parser, client):
+def parse_groups(subparsers):
     return None
 
 
-def group(parent_parser, client):
+def parse_namespaces(subparsers):
     return None
 
 
-def registry(parent_parser, client):
-    return None
+def parse_user(subparsers):
+    user_parser = subparsers.add_parser("user", help="user help")
+    user_subparser = user_parser.add_subparsers()
 
+    # 'user list' subcommand
+    user_create_parser = user_subparser.add_parser("list")
+    user_create_parser.set_defaults(function="list")
 
-def container(parent_parser, client):
-    return None
-
-
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "object",
-        type=str,
-        action="store",
-        help="Type of API content to operate against (user, group, namespace)",
+    # 'user create' subcommand
+    user_create_parser = user_subparser.add_parser("create")
+    user_create_parser.add_argument(
+        "new_user", type=str, help="username of the user to be created."
     )
-    parser.add_argument("operation", type=str, action="store")
-    parser.add_argument("remaining", type=str, action="store", nargs="*")
+    user_create_parser.add_argument(
+        "new_password", type=str, help="password of the user to be created."
+    )
+    user_create_parser.add_argument(
+        "--email", type=str, help="email of the user to be created."
+    )
+    user_create_parser.add_argument(
+        "--first-name", type=str, help="first name of the user to be created."
+    )
+    user_create_parser.add_argument(
+        "--last-name", type=str, help="last name of the user to be created."
+    )
+    user_create_parser.set_defaults(function="create")
+
+    # 'user delete' subcommand
+    user_delete_parser = user_subparser.add_parser("delete")
+    user_delete_parser.add_argument(
+        "selected_user", type=str, help="username of the user to be deleted."
+    )
+    user_delete_parser.set_defaults(function="delete")
+
+
+def test(commant_to_test):
+    parser = argparse.ArgumentParser(prog="galaxykit")
+    subparsers = parser.add_subparsers()
+
     parser.add_argument("-i", "--ignore", default=False, action="store_true")
-    parser.add_argument("-u", "--username", type=str, action="store")
-    parser.add_argument("-p", "--password", type=str, action="store")
+    parser.add_argument("-u", "--username", default="admin", type=str, action="store")
+    parser.add_argument("-p", "--password", default="admin", type=str, action="store")
     parser.add_argument(
         "-c",
         "--ignore-certs",
-        default=False,
         action="store_true",
+        default=False,
         help="Ignore invalid SSL certificates",
     )
     parser.add_argument(
         "-s",
         "--server",
-        type=str,
         action="store",
         default="http://localhost:8002/api/automation-hub/",
+        type=str,
     )
 
-    args = parser.parse_args()
-    ignore = args.ignore
-    https_verify = not args.ignore_certs
-    client = GalaxyClient(
-        args.server, (args.username, args.password), https_verify=https_verify
-    )
-    try:
-        funs = {
-            "user": user,
-            "group": group,
-            "namespace": namespace,
-            "container": container,
-            "collection": collection,
-            "url": url,
-        }
-        if args.object in funs:
-            funs[args.object](args.rest, client)
-        else:
-            print(f"Unknown resource type '{args.object}'")
-            sys.exit(EXIT_UNKNOWN_ERROR)
-    except GalaxyClientError:
-        if not ignore:
-            raise
+    # parsing for subcommands
+    parse_collections(subparsers)
+    parse_containers(subparsers)
+    parse_groups(subparsers)
+    parse_namespaces(subparsers)
+    parse_user(subparsers)
+
+    args = parser.parse_args(command_to_test)
+    print(args)
+
+
+command_to_test = "user create jdoe passwd --email 'jdoe@redhat.com' --first-name John --last-name Doe".split(
+    " "
+)
+test(command_to_test)
