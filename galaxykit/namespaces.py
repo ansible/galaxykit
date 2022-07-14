fac3b17e5dbd642b685f86a970dba7bb887e3376
Christian <ctorrens@redhat.com>
2022-07-14 10:23:47 +0200

    restore group_id

--- galaxykit/namespaces.py
+++ galaxykit/namespaces.py
@@ -1,14 +1,17 @@
 from . import groups
+from .utils import logger
 
 
 def create_namespace(client, name, group):
     try:
         get_namespace(client, name)
     except KeyError:
-        groups = []
+        ns_groups = []
         if group:
-            groups.append(
+            group_id = groups.get_group_id(client, group)
+            ns_groups.append(
                 {
+                    "id": group_id,
                     "name": group,
                     "object_permissions": ["change_namespace", "upload_to_namespace"],
                     "object_roles": [],
@@ -16,8 +19,9 @@ def create_namespace(client, name, group):
             )
         create_body = {
             "name": name,
-            "groups": groups,
+            "groups": ns_groups
         }
+        logger.debug(f"Creating namespace {name}. Request body {create_body}")
         return client.post("v3/namespaces/", create_body)
     else:
         if group:
