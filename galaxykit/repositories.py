ba569370bd15a8e34492f171f2fc98a2b857ea41
Christian <ctorrens@redhat.com>
2023-05-09 16:48:57 +0200

    fix url for x-repo search add option to upload directly to repo (not through staging)

--- galaxykit/repositories.py
+++ galaxykit/repositories.py
@@ -1,6 +1,5 @@
 from . import remotes
 from . import utils
-from . import tasks
 from galaxykit.utils import wait_for_task
 from urllib.parse import urljoin
 
@@ -77,11 +76,7 @@ def put_update_repository(client, repository_id, update_body):
 
 
 def search_collection(client, **search_param):
-    galaxy_root = client.galaxy_root.split("api/automation-hub/")[0]
-    search_url = (
-        f"{galaxy_root}pulp_ansible/galaxy/default/api/v3/"
-        f"plugin/ansible/search/collection-versions/?"
-    )
+    search_url = "v3/plugin/ansible/search/collection-versions/?"
     for key, value in search_param.items():
         if isinstance(value, list):
             param = "&".join([f"{key}={v}" for v in value])
