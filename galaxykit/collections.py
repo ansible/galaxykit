44bf4438fb472a0906e0cef7f3557de752571b2b
Christian <ctorrens@redhat.com>
2023-12-18 16:03:00 +0100

    add collection methods No-Issue

--- galaxykit/collections.py
+++ galaxykit/collections.py
@@ -23,6 +23,15 @@ def get_collection(client, namespace, collection_name, version):
     collection_url = f"v3/collections/{namespace}/{collection_name}/versions/{version}/"
     return client.get(collection_url)
 
+def get_collection_from_repo(client, repository, namespace, collection_name, version):
+    collection_url = (f"content/{repository}/v3/collections/"
+                      f"{namespace}/{collection_name}/versions/{version}/")
+    return client.get(collection_url)
+
+def get_ui_collection(client, repository, namespace, collection_name, version):
+    ui_collection_url = (f"_ui/v1/repo/{repository}/{namespace}/"
+                         f"{collection_name}/?versions={version}")
+    return client.get(ui_collection_url)
 
 def get_collection_list(client):
     url = "_ui/v1/collection-versions/?limit=999999"
@@ -81,6 +90,7 @@ def upload_artifact(
     no_filename=False,
     no_file=False,
     path=None,
+    use_distribution=False
 ):
     """
     Publishes a collection to a Galaxy server and returns the import task URI.
@@ -174,7 +184,10 @@ def upload_artifact(
             f"content/inbound-{artifact.namespace}/v3/artifacts/collections/"
         )
 
-    n_url = urljoin(client.galaxy_root, col_upload_path)
+    if use_distribution:
+        n_url = urljoin(client.galaxy_root, f"content/inbound-{artifact.namespace}/v3/artifacts/collections/")
+    else:
+        n_url = urljoin(client.galaxy_root, col_upload_path)
     resp = client.post(n_url, body=data, headers=headers)
     return resp
 
